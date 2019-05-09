0e497b14ae7c679bd76e7632b235e93e9c5248e6
AlexisGlawitsch <lexiglawitsch@gmail.com>
2019-05-08 22:12:13 -0700

    Updated main bot script as well as testing script.

--- main.py
+++ main.py
@@ -1,62 +1,67 @@
 # Work with Python 3.6
 import discord
+from discord.ext import commands
 import json
 from urllib.request import urlopen
 import random
 
-TOKEN = 'NTcwNjY3NjkzNzA3ODg2NjE0.XMCh4Q.VN0fqVjv7mpnKvxl9M2LL0wISYs'
-prefix = 'g!'
+with open('config.json', 'r') as f:
+    config_dict = json.load(f)
+
+TOKEN = config_dict['token']
 
 client = discord.Client()
+bot = commands.Bot(command_prefix='g!', description="Simulates gacha rolls.")
+cogs = ['greetings']
 
-@client.event
-async def on_message(message):
-    global prefix
+# @client.event
+# async def on_message(message):
+#     global prefix
+#
+#     # we do not want the bot to reply to itself
+#     if message.author == client.user:
+#         return
+#
+#     userMsg = message.content
+#     wordList = userMsg.split()
+#
+#     if wordList[0] == prefix + 'roll':
+#         # Check if char name is valid
+#         if len(wordList) > 1:
+#             # TODO list of valid character names
+#             charName = wordList[1];
+#         # else:
+#             # await message.channel.send(random_card())
+#     elif wordList[0] == prefix + 'inv':
+#         await message.channel.send('You currently have these cards in your inventory:')
+#     elif wordList[0] == prefix + 'help':
+#         if len(wordList) == 1:
+#             helpmsg = 'Here is a list of all available GachaBot commands:\n'
+#             helpmsg += '**Gacha:** inv roll\n'
+#             helpmsg += '**Info:** card idol\n'
+#             helpmsg += '**Bot Management:** defprefix\n\n'
+#             helpmsg += 'For help with individual commands, type ' + prefix + 'help ' +\
+#                 '[command]'
+#             await message.channel.send(helpmsg)
+#         else:
+#             await message.channel.send(command_help(wordList[1]))
+#     elif wordList[0] == prefix + 'defprefix':
+#         if message.author == Guild.owner:
+#             if len(wordList) > 1:
+#                 prefix = wordList[1];
+#                 await message.channel.send('GachaBot prefix has been set to ' +\
+#                     wordList[1])
+#             else:
+#                 await message.channel.send('Invalid format. The correct format ' +\
+#                     'is ' + prefix + 'defprefix [new prefix].')
+#         else:
+#             await message.channel.send('Only the server owner can change GachaBot\'s' +\
+#                 ' prefix.')
+#     else:
+#         await message.channel.send('Invalid command. Type ' + prefix + 'help ' +\
+#             'for help.')
 
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    userMsg = message.content
-    wordList = userMsg.split()
-
-    if wordList[0] == prefix + 'roll':
-        # Check if char name is valid
-        if len(wordList) > 1:
-            # TODO list of valid character names
-            charName = wordList[1];
-        else:
-            await message.channel.send(random_card())
-    elif wordList[0] == prefix + 'inv':
-        await message.channel.send('You currently have these cards in your inventory:')
-    elif wordList[0] == prefix + 'help':
-        if len(wordList) == 1:
-            helpmsg = 'Here is a list of all available GachaBot commands:\n'
-            helpmsg += '**Gacha:** inv roll\n'
-            helpmsg += '**Info:** card idol\n'
-            helpmsg += '**Bot Management:** defprefix\n\n'
-            helpmsg += 'For help with individual commands, type ' + prefix + 'help ' +\
-                '[command]'
-            await message.channel.send(helpmsg)
-        else:
-            await message.channel.send(command_help(wordList[1]))
-    elif wordList[0] == prefix + 'defprefix':
-        if message.author == Guild.owner:
-            if len(wordList) > 1:
-                prefix = wordList[1];
-                await message.channel.send('GachaBot prefix has been set to ' +\
-                    wordList[1])
-            else:
-                await message.channel.send('Invalid format. The correct format ' +\
-                    'is ' + prefix + 'defprefix [new prefix].')
-        else:
-            await message.channel.send('Only the server owner can change GachaBot\'s' +\
-                ' prefix.')
-    else:
-        await message.channel.send('Invalid command. Type ' + prefix + 'help ' +\
-            'for help.')
-
-def random_card():
+async def random_card():
     id_list = json.load(urlopen('http://schoolido.lu/api/cardids/'))
     rnd_id = random.randint(1, len(id_list))
     rnd_card = json.load(urlopen('http://schoolido.lu/api/cards/' + str(rnd_id) + '/'))
@@ -67,8 +72,21 @@ def random_card():
         '\n**Sub Unit:** ' + rnd_idol.get('sub_unit') + '\n**Rarity:** ' +\
         rnd_card.get('rarity') + '\n**Attribute:** ' + str(rnd_card.get('attribute'))
 
-    return cardstr
+    img1 = rnd_card.get('card_image')
+    img2 = rnd_card.get('card_idolized_image')
 
+    images = Attachment(cardstr, img1, img2)
+    # return cardstr
+    await message.channel.send(cardstr)
+
+# @commands.command(
+#     name='test',
+#     description='test command',
+#     aliases=['t']
+# )
+# async def test(ctx, arg1, arg2):
+#     print('test command received')
+#     await ctx.send('You passed {} and {}'.format(arg1, arg2))
 
 def command_help(cmd):
     if cmd == 'inv':
@@ -94,11 +112,28 @@ def command_help(cmd):
 
 @client.event
 async def on_ready():
+    global cogs
+
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
     print('------')
+
+    for cog in cogs:
+        try:
+            bot.load_extension(cog)
+        except Exception as e:
+               print('An error occurred while trying to load the "{}"' + \
+                'extension.\n{}: {}'.format(cog, type(e).__name__, e))
+
+        try:
+            test = bot.get_cog('Greetings')
+            commands = test.get_commands()
+            print([c.name for c in commands])
+        except Exception as e:
+            print("oops")
+
     await client.change_presence(status=discord.Status.online,
-        activity=discord.Game(name='' + prefix + 'help'))
+        activity=discord.Game(name='' + bot.command_prefix + 'help'))
 
 client.run(TOKEN)
