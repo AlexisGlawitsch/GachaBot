ea4dab085d711149b036951a073e297ad5c1806e
AlexisGlawitsch <lexiglawitsch@gmail.com>
2019-05-09 21:54:10 -0700

    Fixed string concatenation error in gacha roll output and added probability functionality in random gacha rolls.

--- cogs/gacha.py
+++ cogs/gacha.py
@@ -15,36 +15,52 @@ class Gacha(commands.Cog):
             # TODO list of valid character names
             charName = args[1];
         else:
-            random_card(self, ctx)
+            rnd_num = random.randint(1, 100)
+
+            if rnd_num <= 80:
+                rarity = 'R'
+            elif rnd_num <= 95:
+                rarity = 'SR'
+            elif rnd_num <= 99:
+                rarity = 'SSR'
+            else:
+                rarity = 'UR'
+
+            id_list = json.load(urlopen('http://schoolido.lu/api/cardids?rarity=' + rarity))
+
+            rnd_id = id_list[random.randint(0, len(id_list) - 1)]
+
+            rnd_card = json.load(urlopen('http://schoolido.lu/api/cards/' + str(rnd_id) + '/'))
+            rnd_idol = rnd_card.get('idol')
+
+            cardstr = '**ID:** ' + str(rnd_card.get('id')) + '\n**Name:** ' +\
+                rnd_idol.get('name')
+            if rnd_idol.get('main_unit') is not None:
+                cardstr += '\n**Main Unit:** ' + rnd_idol.get('main_unit')
+            if rnd_idol.get('sub_unit') is not None:
+                cardstr += '\n**Sub Unit:** ' + rnd_idol.get('sub_unit')
+            cardstr += '\n**Rarity:** ' + rnd_card.get('rarity')
+            if rnd_idol.get('attribute') is not None:
+                cardstr += '\n**Attribute:** ' + str(rnd_card.get('attribute'))
+
+            # Retrieve card image(s) and send with message
+            # url1 = rnd_card.get('card_image')
+            # url2 = rnd_card.get('card_idolized_image')
+            #
+            # img1=urllib.FancyURLopener()
+            # img1.retrieve(url1, str(rnd_card.get('id')) + 'unidolized')
+            #
+            # img2=urllib.FancyURLopener()
+            # img2.retrieve(url2, str(rnd_card.get('id')) + 'idolized')
+            #
+            #
+            # images = Attachment(cardstr, img1, img2)
+            # return cardstr
+            await ctx.send(cardstr)
 
     @commands.command()
     async def inv(self, ctx):
         await ctx.send('Here is a list of the cards in your inventory:')
-
-    async def random_card(self, ctx):
-        id_list = json.load(urlopen('http://schoolido.lu/api/cardids/'))
-        rnd_id = random.randint(1, len(id_list))
-        rnd_card = json.load(urlopen('http://schoolido.lu/api/cards/' + str(rnd_id) + '/'))
-        rnd_idol = rnd_card.get('idol')
-
-        cardstr = '**ID:** ' + str(rnd_card.get('id')) + '\n**Name:** ' +\
-            rnd_idol.get('name') + '\n**Main Unit:** ' + rnd_idol.get('main_unit') +\
-            '\n**Sub Unit:** ' + rnd_idol.get('sub_unit') + '\n**Rarity:** ' +\
-            rnd_card.get('rarity') + '\n**Attribute:** ' + str(rnd_card.get('attribute'))
-
-        # url1 = rnd_card.get('card_image')
-        # url2 = rnd_card.get('card_idolized_image')
-        #
-        # img1=urllib.FancyURLopener()
-        # img1.retrieve(url1, str(rnd_card.get('id')) + 'unidolized')
-        #
-        # img2=urllib.FancyURLopener()
-        # img2.retrieve(url2, str(rnd_card.get('id')) + 'idolized')
-        #
-        #
-        # images = Attachment(cardstr, img1, img2)
-        # return cardstr
-        ctx.send(cardstr)
 
 def setup(bot):
     bot.add_cog(Gacha(bot))
