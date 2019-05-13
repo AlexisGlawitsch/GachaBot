8b63ee70b5e192e2d0435c71517d568a449074eb
AlexisGlawitsch <lexiglawitsch@gmail.com>
2019-05-13 14:26:41 -0700

    Removed testing code in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Work with Python 3.6
 import discord
 from discord.ext import commands
+from discord.ext.commands import CommandNotFound
 import json
 
 # Get bot token from config.json
@@ -10,9 +11,17 @@ with open('config.json', 'r') as f:
 TOKEN = config_dict['token']
 
 bot = commands.Bot(command_prefix='g!', description="Simulates gacha rolls.")
-cogs = ['cogs.basic', 'cogs.info', 'cogs.gacha', 'cogs.config']
+cogs = ['cogs.basic', 'cogs.info', 'cogs.gacha', 'cogs.stats', 'cogs.config']
 
 bot.remove_command('help')
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, CommandNotFound):
+        await ctx.send('Invalid command. For a list of valid commands, type `' +
+            bot.command_prefix + 'help`.')
+    else:
+        raise error
 
 @bot.event
 async def on_ready():
@@ -26,9 +35,10 @@ async def on_ready():
     if __name__ == '__main__':
         try:
             for cog in cogs:
+                current_cog = cog
                 bot.load_extension(cog)
         except discord.ext.commands.errors.ExtensionAlreadyLoaded:
-            pass
+            bot.reload_extension(current_cog)
 
     await bot.change_presence(status=discord.Status.online,\
         activity=discord.Game(name='' + bot.command_prefix + 'help'))
