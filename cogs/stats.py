ec4fa4b2ba3a6d1a8671922d473cead0f0a9c4ea
AlexisGlawitsch <lexiglawitsch@gmail.com>
2019-05-13 14:27:10 -0700

    Added temporary stats command

--- cogs/stats.py
+++ cogs/stats.py
@@ -9,10 +9,18 @@ class Stats(commands.Cog):
     @commands.command()
     async def profile(self, ctx):
         await ctx.send('Here is your profile')
-        
+
     @commands.command()
     async def inv(self, ctx):
         await ctx.send('Here is a list of the cards in your inventory:')
+
+    @commands.command()
+    async def stats(self, ctx):
+        msg = 'Rolled Cards:\n\nLove Live! School Idol Festival!\n'
+        msg += 'UR: 0\nSSR: 0\nSR: 0\nR: 0\n\nBang Dream! Girls Band Party\n'
+        msg += '4 Star: 0\n3 Star: 0\n2 Star: 0'
+        
+        await ctx.send(msg)
 
 def setup(bot):
     bot.add_cog(Stats(bot))
